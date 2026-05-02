48538f77a1ef6da0cbbcae9fed4e66e275b6db66
fedddot <idanrommm@gmail.com>
2026-05-02 20:02:12 +0000

    Update CMake configuration to include esp-tls component and refactor esp-server to use WifiStationGuard with blink_loop_x

--- src/esp-server.cpp
+++ src/esp-server.cpp
@@ -5,13 +5,13 @@
 #include "driver/gpio.h"
 
 #include "http_parser.h"
-#include "wifi_access_point_guard.hpp"
 #include "http_request_data_reader.hpp"
 #include "http_request_data_writer.hpp"
 #include "pb_message_reader.hpp"
 #include "pb_message_writer.hpp"
 
 #include "service.pb.h"
+#include "wifi_station_guard.hpp"
 
 #ifndef NETWORK_SSID
 #  error "NETWORK_SSID is not defined. Please pass it to the cmake command"
@@ -76,12 +76,13 @@ static esp_err_t config_wifi_cb(httpd_req_t *request) {
 }
 
 static void blink_loop();
+static void blink_loop_x();
 
 extern "C" {
     void app_main(void) {
         try {
             mcu_server::NvsFlashGuard nvs_guard;
-            mcu_server::WifiAccessPointGuard network_guard(NETWORK_SSID, NETWORK_PASSWORD, nvs_guard);
+            mcu_server::WifiStationGuard network_guard(NETWORK_SSID, NETWORK_PASSWORD, nvs_guard, blink_loop_x);
             const auto get_handler = httpd_uri_t {
                 .uri      = "/config/wifi",
                 .method   = HTTP_POST,
@@ -136,8 +137,27 @@ inline void blink_loop() {
     gpio_config(&io_conf);
 
     int led_on_time = 100;
-    int led_off_time = 1900;
+    int led_off_time = 100;
     while (true) {
+        gpio_set_level(GPIO_NUM_15, true);
+        vTaskDelay(pdMS_TO_TICKS(led_on_time));
+        gpio_set_level(GPIO_NUM_15, false);
+        vTaskDelay(pdMS_TO_TICKS(led_off_time));
+    }
+}
+
+inline void blink_loop_x() {
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_DISABLE;
+    io_conf.mode = GPIO_MODE_OUTPUT;
+    io_conf.pin_bit_mask = (1ULL << GPIO_NUM_15);
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
+    gpio_config(&io_conf);
+
+    int led_on_time = 100;
+    int led_off_time = 1900;
+    for (int i = 0; i < 2; ++i) {
         gpio_set_level(GPIO_NUM_15, true);
         vTaskDelay(pdMS_TO_TICKS(led_on_time));
         gpio_set_level(GPIO_NUM_15, false);
