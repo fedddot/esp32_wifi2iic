3f4527995d32794b2238f8d9c68727fba49df078
fedddot <idanrommm@gmail.com>
2026-05-02 07:40:03 +0000

    Update CMakeLists.txt and esp-server.cpp to include esp_netif; implement AP configuration and HTTP response handling

--- src/esp-server.cpp
+++ src/esp-server.cpp
@@ -1,6 +1,9 @@
 #include <stdexcept>
 
 #include "esp_http_server.h"
+#include "esp_wifi.h"
+#include "esp_wifi_types_generic.h"
+#include "nvs_flash.h"
 
 static httpd_handle_t start_webserver(const httpd_uri_t& get_handler) {
     httpd_config_t config = HTTPD_DEFAULT_CONFIG();
@@ -15,12 +18,77 @@ static httpd_handle_t start_webserver(const httpd_uri_t& get_handler) {
     return server;
 }
 
-static esp_err_t get_handler_cb(httpd_req_t *req) {
-    throw std::runtime_error("not implemented");
+static esp_err_t get_handler_cb(httpd_req_t *request) {
+    if (const auto res = httpd_resp_set_status(request, HTTPD_200); res != ESP_OK) {
+        return res;
+    }
+    if (const auto res = httpd_resp_set_type(request, HTTPD_TYPE_OCTET); res != ESP_OK) {
+        return res;
+    }
+    const std::string report_body = "hey there";
+    return httpd_resp_send(request, report_body.data(), report_body.size());
+}
+
+static wifi_ap_config_t generate_ap_cfg(const std::string& ssid, const std::string& password) {
+    auto config = wifi_ap_config_t {
+        .ssid = {'\0'},
+        .password = {'\0'},
+        .ssid_len = 0,
+        .channel = 0, // TODO: check it
+        .authmode = WIFI_AUTH_WPA2_PSK,
+        .ssid_hidden = 0,
+        .max_connection = 1,
+        .beacon_interval = 100,
+        .csa_count = 3,
+        .dtim_period = 1,
+        .pairwise_cipher = WIFI_CIPHER_TYPE_NONE,
+        .ftm_responder = false,
+        .pmf_cfg = wifi_pmf_config_t {
+            .capable = false,
+            .required = false
+        },
+        .sae_pwe_h2e = WPA3_SAE_PWE_UNSPECIFIED,
+        .transition_disable = 0,
+        .sae_ext = 0,
+        .bss_max_idle_cfg = wifi_bss_max_idle_config_t {
+            .period = 0,
+            .protected_keep_alive = false
+        },
+        .gtk_rekey_interval = 0
+    };
+    if (ssid.size() > sizeof(config.ssid)) {
+        throw std::invalid_argument("received ssid is too long");
+    }
+    if (ssid.size() != ssid.copy((char *)(config.ssid), ssid.size())) {
+        throw std::runtime_error("failed to create AP config: SSID copy failure");
+    }
+    config.ssid_len = ssid.size();
+    if (password.size() > sizeof(config.password)) {
+        throw std::invalid_argument("received password is too long");
+    }
+    if (password.size() != password.copy((char *)(config.password), password.size())) {
+        throw std::runtime_error("failed to create AP config: Password copy failure");
+    }
+    return config;
 }
 
 extern "C" {
     void app_main(void) {
+        const std::string& ssid = "ESP32-AP";
+        const std::string& password = "EspPassWord";
+        ESP_ERROR_CHECK(nvs_flash_init());
+        ESP_ERROR_CHECK(esp_netif_init());
+        ESP_ERROR_CHECK(esp_event_loop_create_default());
+        esp_netif_create_default_wifi_ap();
+        wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+        ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+        wifi_config_t ap_config = {
+            .ap = generate_ap_cfg(ssid, password)
+        };
+        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+        ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &ap_config));
+        ESP_ERROR_CHECK(esp_wifi_start());
+
         const auto get_handler = httpd_uri_t {
             .uri      = "/some/data",
             .method   = HTTP_GET,
