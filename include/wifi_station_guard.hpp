da9b13e0c87a51e771f99eac7d95310b5548a91d
fedddot <idanrommm@gmail.com>
2026-05-02 20:01:44 +0000

    Refactor WifiStationGuard constructor to include error callback and improve error handling during initialization

--- include/wifi_station_guard.hpp
+++ include/wifi_station_guard.hpp
@@ -12,23 +12,40 @@
 namespace mcu_server {
     class WifiStationGuard {
     public:
-        WifiStationGuard(const std::string& ssid, const std::string& password, const NvsFlashGuard& nvs_guard): m_nvs_guard(nvs_guard) {
+        WifiStationGuard(const std::string& ssid, const std::string& password, const NvsFlashGuard& nvs_guard, void (*err_callback)(void)): m_nvs_guard(nvs_guard), m_netif(nullptr) {
             if (s_reference_count) {
                 throw std::runtime_error("WifiStationGuard is already initialized");
             }
-            ESP_ERROR_CHECK(esp_netif_init());
-            ESP_ERROR_CHECK(esp_event_loop_create_default());
-            esp_netif_create_default_wifi_sta();
+
+            if (esp_netif_init() != ESP_OK) {
+                throw std::runtime_error("Failed to initialize network interface");
+            }
+            if (esp_event_loop_create_default() != ESP_OK) {
+                throw std::runtime_error("Failed to create default event loop");
+            }
+            m_netif = esp_netif_create_default_wifi_sta();
+            if (m_netif == nullptr) {
+                throw std::runtime_error("Failed to create default Wi-Fi station");
+            }
             wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-            ESP_ERROR_CHECK(esp_wifi_init(&cfg));
-            wifi_config_t sta_config = {
-                .sta = generate_sta_config(ssid, password)
-            };
-            sta_config.sta.threshold.authmode = WIFI_AUTH_WPA2_PSK;
-            ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-            ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &sta_config));
-            ESP_ERROR_CHECK(esp_wifi_start());
-            ESP_ERROR_CHECK(esp_wifi_connect());
+            if (esp_wifi_init(&cfg) != ESP_OK) {
+                throw std::runtime_error("Failed to initialize Wi-Fi");
+            }
+
+            if (esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, WifiStationGuard::event_handler, (void *)err_callback, nullptr) != ESP_OK) {
+                throw std::runtime_error("Failed to register Wi-Fi event handler");
+            }
+            if (esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, WifiStationGuard::event_handler, (void *)err_callback, nullptr) != ESP_OK) {
+                throw std::runtime_error("Failed to register IP event handler");
+            }
+
+            wifi_config_t wifi_config = {};
+            std::memset(&wifi_config, 0, sizeof(wifi_config));
+            ssid.copy((char*)wifi_config.sta.ssid, ssid.size());
+            password.copy((char*)wifi_config.sta.password, password.size());
+            esp_wifi_set_mode(WIFI_MODE_STA);
+            esp_wifi_set_config(WIFI_IF_STA, &wifi_config);
+            esp_wifi_start();
             ++s_reference_count;
         }
         WifiStationGuard(const WifiStationGuard&) {
@@ -47,23 +64,20 @@ namespace mcu_server {
         }
     private:
         NvsFlashGuard m_nvs_guard;
+        esp_netif_t *m_netif;
         static std::size_t s_reference_count;
 
-        static wifi_sta_config_t generate_sta_config(const std::string& ssid, const std::string& password) {
-            auto config = wifi_sta_config_t {};
-            if (ssid.size() > sizeof(config.ssid)) {
-                throw std::invalid_argument("received ssid is too long");
+        static void event_handler(void* arg, esp_event_base_t event_base, int32_t event_id, void* event_data) {
+            if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+                esp_wifi_connect();
+            } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+                esp_wifi_connect(); // Reconnect
+            } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+                return;
+                // Connected!
             }
-            if (ssid.size() != ssid.copy((char *)(config.ssid), ssid.size())) {
-                throw std::runtime_error("failed to create STA config: SSID copy failure");
-            }
-            if (password.size() > sizeof(config.password)) {
-                throw std::invalid_argument("received password is too long");
-            }
-            if (password.size() != password.copy((char *)(config.password), password.size())) {
-                throw std::runtime_error("failed to create STA config: Password copy failure");
-            }
-            return config;
+            auto cb = (void (*)(void))arg;
+            cb();
         }
     };
 
